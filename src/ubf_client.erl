37ecfbb7c4ce123d21e58c9f68a14c3c56233d82
Scott <fritchie@bb3.(none)>
2009-04-19 21:39:22 -0500

    Second round of EDoc additions

--- src/ubf_client.erl
+++ src/ubf_client.erl
@@ -1,29 +1,80 @@
+%%%
+%%% @doc UBF client-side public API.
+%%%
+%%% This module implements most of the commonly-used client-side
+%%% functions required to talk to UBF servers: connect() to a UBF
+%%% server, rpc() to make a synchronous call to a connected UBF
+%%% server, stop() a connection, and install_handler() to add a
+%%% callback function to handle asynchronous notifications from the
+%%% UBF server to your client process.
+%%%
+%%% See the documentation for the <tt>file_plugin</tt> module for
+%%% extra commentary on writing an UBF server implementation module.
+%%%
+
 -module(ubf_client).
 
 %% -compile(export_all).
 
-%% start(Host,Port)  -> {ok, Pid, Service} | {error, What}
+%% connect(Host,Port)  -> {ok, Pid, Service} | {error, What}
 %% rpc(Pid, Q) -> Reply
 %% stop(Pid)   -> ack.
 
--export([connect/2, connect/3, connect/4, rpc/2, rpc/3, stop/1, install_default_handler/1,
-         install_handler/2]).
+-export([connect/2, connect/3, connect/4, rpc/2, rpc/3, stop/1,
+	 install_default_handler/1, install_handler/2]).
 
 -import(ubf_utils, [spawn_link_debug/2]).
 
+%% @type address() = string() | ip_address(). A DNS hostname or IP address.
+%% @type connect_options() = list(ubf | ebf | jsf).
+%%       An OTP-style property list, see 'proplists' module for details.
+%% @type ip_address() = string() | tuple().  An IP address in string form,
+%%       e.g. "127.0.0.1" (IPv4) or "::1" (IPv6), or in tuple form (see
+%%       documentation for Erlang's 'inet' module for details).
+%% @type plugin_module_list() = list(atom()).  A list of plugin module names
+%%       that will be passed to ubf_plugin_meta_serverful:new() or
+%%       ubf_plugin_meta_serverless:new() for client initialization.
+%% @type tcp_port() = integer().  A TCP port number.
+%% @type timeout() = integer() | infinity.  An Erlang-style timeout value.
+
+%% @spec (string(), list()) -> term()
+%% @doc Debugging function, uncomment the alternate form for
+%% io:format()/"printf()"-style debugging messages.
+
 debug(_F, _S) -> true.
 %% debug(F, S) -> io:format(F, S).
+
+%% @spec (address(), tcp_port()) ->
+%%       {ok, pid(), service()} | {error, term()}
+%% @doc Connect to a UBF server at address Host + TCP port Port.
 
 connect(Host, Port)
   when is_list(Host) andalso is_integer(Port) ->
     connect(Host, Port, 10000).
 
+%% @spec (address(), tcp_port(), timeout()) ->
+%%       {ok, pid(), service()} | {error, term()}
+%% @doc Connect to a UBF server at address Host + TCP port Port.
+
 connect(Host, Port, Timeout)
   when is_list(Host) andalso is_integer(Port) ->
     connect(Host, Port, [], Timeout).
 
+%% @spec (address() | plugin_module_list(), tcp_port() | pid() | atom(),
+%%        timeout(), proplist()) ->
+%%       {ok, pid(), service()} | {error, term()}
+%% @doc Connect to a UBF server at address Host + TCP port Port, or at
+%%      pid/registered name Server.
+%%
+%% When using the alternate form, the first two arguments are:
+%% <ol>
+%% <li> Plugins: a plugin_module_list(). </li>
+%% <li> Server: either a process id (pid()) or process registered
+%%      name (atom()) for an already-started UBF server. </li>
+%% </ol>
+
 connect(Host, Port, Options, Timeout)
-  when is_list(Host) andalso is_integer(Port) andalso is_list(Options) ->
+  when is_integer(Port) andalso is_list(Options) ->
     Self = self(),
     Pid = spawn_link_debug(ubf_client, fun() -> ubf_client(Self, Host, Port, Options, Timeout) end),
     receive
@@ -55,7 +106,8 @@ connect(Plugins, Server, Options, Timeout)
             Error
     end.
 
-%% This executes inside the ubf_client
+%% @doc This executes inside the ubf_client
+
 ubf_client(Parent, Host, Port, Options, Timeout)
   when is_list(Host) andalso is_integer(Port) andalso is_list(Options) ->
     process_flag(trap_exit, true),
@@ -114,12 +166,24 @@ drop_fun(_Msg) ->
     %% io:format("GOT A CALLBACK HORRAY:~p~n",[Msg]),
     fun drop_fun/1.
 
+%% @spec (pid()) -> ok
+%% @doc Stop a UBF client process.
+
 stop(Pid) ->
     Pid ! stop,
     ok.
 
+%% @spec (pid(), term()) -> timeout | term()
+%% @doc Perform a synchronous RPC call.
+%%
+%% NOTE: It is not recommended that a UBF client return the
+%% bare atom 'timeout' in response to any RPC call.
+
 rpc(Pid, Q) ->
     rpc(Pid, Q, infinity).
+
+%% @spec (pid(), term(), timeout()) -> timeout | term()
+%% @doc Perform a synchronous RPC call.
 
 rpc(Pid, Q, Timeout) ->
     Pid ! {self(), {rpc, Q}},
@@ -141,8 +205,31 @@ rpc(Pid, Q, Timeout) ->
             end
     end.
 
+%% @spec (pid()) -> ack
+%% @doc Install a default handler function (callback-style) for
+%% asynchronous UBF messages.
+%%
+%% The default handler function, drop_fun/1, does nothing.
+
 install_default_handler(Pid) ->
     install_handler(Pid, fun drop_fun/1).
+
+%% @spec (pid(), function()) -> ack
+%% @doc Install a handler function (callback-style) for asynchronous
+%% UBF messages.
+%%
+%% The handler fun Fun should be an function of arity 1.  When an
+%% asynchronous UBF message is received, the callback function will be
+%% called with the UBF message as its single argument.
+%%
+%% If your handler fun must maintain its own state, then you must use
+%% an intermediate anonymous fun to bind the state.  See the usage of
+%% the <tt>irc_client_gs:send_self/2</tt> fun as an example.  The
+%% <tt>send_self()</tt> fun is actually arity 2, but the extra
+%% argument is how the author, Joe Armstrong, maintains the extra
+%% state required to deliver the async UBF message to the process that
+%% is executing the event loop processing function,
+%% <tt>irc_client_gs:loop/6</tt>.
 
 install_handler(Pid, Fun) ->
     Pid ! {self(), {install, Fun}},
@@ -151,8 +238,12 @@ install_handler(Pid, Fun) ->
             Reply
     end.
 
+%% @doc Entry function for the UBF client process.
+
 ubf_client_loop(Driver) ->
     loop(Driver, fun drop_fun/1).
+
+%% @doc Main loop for the UBF client process.
 
 loop(Driver, Fun) ->
     receive
@@ -198,6 +289,8 @@ loop(Driver, Fun) ->
             io:format("*** YY Client loop dropping:~p~n",[X]),
             loop(Driver, Fun)
     end.
+
+%% @doc Intermediate client shutdown function.
 
 wait_terminate() ->
     %% io:format("In wait terminate:~n"),
--- src/ubf_server.erl
+++ src/ubf_server.erl
@@ -1,3 +1,51 @@
+%%%
+%%% @doc UBF server-side public API.
+%%%
+%%% TO-DO: JoeNorton, I can't seem to start multiple listeners, only
+%%% the first one succeeds.  At a casual glance, that seems to be The
+%%% Current Correct Behavior, but it'd be awful nice to have multiple
+%%% listeners inside the same VM.  Is there another way to do that?
+%%% ubf_server:start([file_plugin], 2000, []).
+%%% ubf_server:start([irc_plugin], 2001, []).
+%%%
+%%% TO-DO: JoeNorton, ubf_server:start([file_plugin], 2000, [jsf]) will
+%%% give a JSON-speaking server that has a lot of difficulty decoding
+%%% and encoding stuff.  Do you have any plan to fix it?
+%%%
+%%% This module implements most of the commonly-used server-side
+%%% functions: starting TCP listeners and registering their
+%%% implementation callback modules.
+%%%
+%%% We implement three different wire formats for accessing the same
+%%% implementation of a UBF(B) protocol-checking server:
+%%%
+%%% <ul>
+%%% <li> UBF(A).  This is Joe Armstrong's original implementation.
+%%%      See <a href="http://www.sics.se/~joe/ubf/">
+%%%      http://www.sics.se/~joe/ubf/</a> for details. </li>
+%%% <li> EBF, a.k.a. Erlang Binary Format.  This protocol uses common
+%%%      Erlang wire formats, the <tt>{packet, 4}</tt> protocol from
+%%%      'inets' for TCP connections, and the
+%%%      <tt>term_to_binary()</tt>/<tt>binary_to_term()</tt> BIFs for
+%%%      payload encoding.  These wire formats are used to pass Erlang
+%%%      terms between a UBF(B) contract checking server and a client
+%%%      that does not support the UBF(A) wire format but does support
+%%%      Erlang's native wire formats. </li>
+%%% <li> JSF, a.k.a the JSON Server Format.  Similar to EBF, except
+%%%      that JavaScript's JSON encoding is used for the wire protocol
+%%%      instead of UBF(A) or Erlang's native wire formats.
+%%%      NOTE: This server is currently incomplete.  Source code from
+%%%            Gemini Mobile Technologies, Inc. is not yet available to
+%%%            help glue JSF-style encoding to a full HTTP/JSON-RPC service.
+%%%      </li>
+%%% </ul>
+%%%
+%%% TO-DO: There is no "stop" function.
+%%%
+%%% See the documentation for the <tt>file_plugin</tt> module for
+%%% extra commentary on writing an UBF server implementation module.
+%%%
+
 -module(ubf_server).
 
 -export([start/2, start/3, start_link/2, start_link/3, init/4, ask_manager/2, sendEvent/2]).
@@ -9,17 +57,66 @@
 -import(ubf_utils, [spawn_link_debug/2]).
 -import(proc_socket_server, [start_raw_server/5]).
 
-%% Here we start the server
-%% This is the *only* registered process on the server side
+%% @spec (list(atom()), integer()) -> true
+%% @doc Start a TCP listener on port Port and register all of the
+%% protocol implementation modules in the PluginModules list.
+%%
+%% Here we start the server.
+%% This is the *only* registered process on the server side.
 
 start(PluginModules, Port) ->
     start(PluginModules, Port, []).
 
+%% @spec (list(atom()), integer(), proplist()) -> true
+%% @doc Start a TCP listener on port Port with the Options properties
+%% list and register all of the protocol implementation modules in the
+%% PluginModules list.
+%%
+%% Valid properties in the Options proplist are:
+%% <ul>
+%% <li> {ebf, true | false} ... Enable the EBF version of the protocol's
+%%      wire format.
+%%      Only one of ebf, jsf, and ubf should be specified.
+%%      Default: false. </li>
+%% <li> {idletimer, integer() | infinity} ... Maximum time (in milliseconds)
+%%      that a client connection may remain idle before the server will
+%%      close the connection.
+%%      Default: infinity </li>
+%% <li> {maxconn, integer()} ... Maximum number of simultaneous TCP
+%%      connections allowed.
+%%      Default: 10,000. </li>
+%% <li> {jsf, true | false} ... Enable the JSON version of the protocol's
+%%      wire format.
+%%      Only one of ebf, jsf, and ubf should be specified.
+%%      Default: false. </li>
+%% <li> {serverhello, string()} ... Meta contract greeting string, sent
+%%      when a client first connects to the server.
+%%      Default: "meta_server" </li>
+%% <li> {statelessrpc, true | false} ... run the stateless variety of
+%%      a UBF(B) contract.  A stateless contract is an extension of
+%%      Joe Armstrong's original UBF server implementation.
+%%      Default: false.
+%%      TO-DO: JoeNorton, add more? </li>
+%% <li> {ubf, true | false} ... Enable the JSON version of the protocol's
+%%      wire format.
+%%      Only one of ebf, jsf, and ubf should be specified.
+%%      Default: true. </li>
+%% <li> {verboserpc, true | false} ... Set the verbose RPC mode.
+%%      Default: false.
+%%      TO-DO: JoeNorton, add more? </li>
+%% </ul>
+
 start(PluginModules, Port, Options) ->
     start_registered(ubf_server, fun() -> start_server(PluginModules, Port, Options) end).
 
+%% @spec (list(atom()), integer()) -> true
+%% @doc See start/2, but also link the server processs to the caller.
+
 start_link(PluginModules, Port) ->
     start_link(PluginModules, Port, []).
+
+%% @spec (list(atom()), integer(), proplist()) -> true
+%% @doc See start/3, but also link the server processs to the caller.
 
 start_link(PluginModules, Port, Options) ->
     proc_lib:start_link(?MODULE, init, [self(), PluginModules, Port, Options]).
