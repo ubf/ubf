8720353f45d5e6544f0696258c755d6b913bb5bb
Joseph Wayne Norton <norton@alum.mit.edu>
2010-12-30 19:02:31 +0900

    Misc. dialyzer warning fixes

--- src/ebf.erl
+++ src/ebf.erl
@@ -17,25 +17,31 @@ proto_packet_type() -> 4.
 
 
 %%---------------------------------------------------------------------
+-spec encode(term()) -> no_return().
 encode(X) ->
     encode(X, ?MODULE).
 
+-spec encode(term(), module()) -> no_return().
 encode(_X, _Mod) ->
     %% see ebf_driver.erl
     exit(notimplemented).
 
 
 %%---------------------------------------------------------------------
+-spec decode(binary()) -> no_return().
 decode(X) ->
     decode(X, ?MODULE).
 
+-spec decode(binary(), module()) -> no_return().
 decode(X, Mod) ->
     decode(X, Mod, decode_init()).
 
+-spec decode(binary(), module(), term()) -> no_return().
 decode(_X, _Mod, _Cont) ->
     %% see ebf_driver.erl
     exit(notimplemented).
 
+-spec decode_init() -> no_return().
 decode_init() ->
     %% see ebf_driver.erl
     exit(notimplemented).
